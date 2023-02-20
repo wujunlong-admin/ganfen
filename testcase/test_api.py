c7b73d970ed4da66378c401ad0f4e270abc87931
mumu <972947181@qq.com>
2023-02-20 13:05:20 +0800

    test-002

--- testcase/test_api.py
+++ testcase/test_api.py
@@ -5,6 +5,8 @@ from API.wesApi import WesApi, TesApi
 log = get_log(str(datetime.datetime.now().date()) + 'case.log')
 class TestApi:
     # 有箱码入库
-    def test_01_inpot_task_code(self, basic_data):
-        log.info('容器规格为:' + basic_data[0], 'sku号为:' + basic_data[1])
-        return WesApi().inpot_task_code(basic_data[0], basic_data[1])
+    def test_01_inpot_task_code(self):
+        log.info('这是一个INFO信息')
+        return "这是一个测试方法"
+        # log.info('容器规格为:' + basic_data[0], 'sku号为:' + basic_data[1])
+        # return WesApi().inpot_task_code(basic_data[0], basic_data[1])
